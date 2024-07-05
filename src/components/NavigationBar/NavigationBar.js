14fec9f4c265111237d17d7cd51c1dcf7d625432
alexcruz097 <alexc017@yahoo.com>
2024-07-05 10:04:25 -0700

    added navbar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,13 +1,35 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from "./NavigationBar.module.css";
+import earth from "./images/earth.png";
+import rocket from "./images/rocket.png";
+import planets from "./images/planets.png";
+function NavigationBar() {
+  let textDecoration = {
+    textDecoration: "none",
+  };
 
-function NavigationBar ()
-{
   return (
-    {
-      // todo
-    }
+    <nav className={styles.navbar}>
+      <NavLink to={"/"} style={textDecoration}>
+        <p>
+          {" "}
+          <img src={earth} />
+          Home{" "}
+        </p>
+      </NavLink>
+      <NavLink to={"/spacecrafts"} style={textDecoration}>
+        <p>
+          <img src={rocket} /> Spacecrafts{" "}
+        </p>
+      </NavLink>
+      <NavLink to={"/planets"} style={textDecoration}>
+        <p>
+          <img src={planets} /> Planets{" "}
+        </p>
+      </NavLink>
+    </nav>
+    // todo
   );
 }
 
